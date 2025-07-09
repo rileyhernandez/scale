56381812540999188f3bcbb7cfada20530ffd4c8
Riley <rileykhernandez@gmail.com>
2025-07-09 14:42:15 -0700

    first commit

---
+++ src/error.rs
@@ -0,0 +1,15 @@
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("Phidget Error: {0}")]
+    Phidget(#[from] phidget::Error),
+    #[error("Reqwest Error: {0}")]
+    Reqwest(#[from] reqwest::Error),
+    #[error("Serde Json Error: {0}")]
+    SerdeJson(#[from] serde_json::Error),
+    #[error("Menu Error: {0}")]
+    Menu(#[from] menu::error::Error),
+    #[error("Failed to start scale")]
+    Initialization,
+    #[error("IO Error: {0}")]
+    Io(#[from] std::io::Error),
+}
