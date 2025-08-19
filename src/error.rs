6065ff41c0130047d09ea34b8405fa66f6923c2e
Riley <rileykhernandez@gmail.com>
2025-08-19 11:48:10 -0700

    removed unused time and reqwest crates

--- src/error.rs
+++ src/error.rs
@@ -2,8 +2,6 @@
 pub enum Error {
     #[error("Phidget Error: {0}")]
     Phidget(#[from] phidget::Error),
-    #[error("Reqwest Error: {0}")]
-    Reqwest(#[from] reqwest::Error),
     #[error("Serde Json Error: {0}")]
     SerdeJson(#[from] serde_json::Error),
     #[error("Menu Error: {0}")]
